23b26de537829b1bbd9d16c72d1e7a4d2e8f759b
durgasaikalyan <durgasaikalyanccbp@gmail.com>
2023-08-22 07:26:08 +0000

    your message

---
+++ src/components/MusicItem/styledComponents.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const MusicListItem = styled.li``
+export const TrackInfoContainer = styled.div``
+export const MusicTrackImage = styled.img``
+export const NameGenreContainer = styled.div``
+export const Genre = styled.p``
+export const DurationDeleteContainer = styled.div``
+export const Duration = styled.p``
+export const DeleteButton = styled.button``
+export const Name = styled.p``
---
+++ src/components/MusicPlayList/styledComponents.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const BgContainer = styled.div``
+export const ArtistBg = styled.div``
+export const ArtistName = styled.h1``
+export const ArtistInfo = styled.p``
+export const PlayListContainer = styled.div``
+export const PlayListHeading = styled.h1``
+export const SearchInput = styled.input``
+export const MusicList = styled.ul``
+export const EmptyContainer = styled.div``
+export const NoSongsText = styled.p``
